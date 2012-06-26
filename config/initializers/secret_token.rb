1e17d022fa5c7c1682ee8f17e08e4ed2c47c076c
unknown <ztt@ztt-THINK.(none)>
2012-06-26 22:33:21 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '63fa3c33d2201d6a49959eee878f58831fdfd4f036df72294afab4c3e290886c6386c646ef3b717bd2ded7962bdf88138a1a40d2ff086d2d39914f2073202b33'
