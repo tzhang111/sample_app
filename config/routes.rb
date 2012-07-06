b7c0b370e0bf6880c9a93f8e862160436bf6ba39
tzhang <skineffect@163.com>
2012-07-06 06:49:58 -0400

    Sign in...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
 match '/signup', :to => 'users#new'
 match '/contact', :to => 'pages#contact'
