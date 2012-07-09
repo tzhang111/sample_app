14c1ddf37b97689ca2e25e47d034d579e361569f
tzhang <skineffect@163.com>
2012-07-08 22:43:24 -0400

    signin/out ...

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -45,13 +45,24 @@ render_views
       
       it "should sign the user in" do
         post :create, :session => @attr
-        
+        controller.current_user.should == @user
+        controller.should be_signed_in
       end
+      
       
       it "should redirect to the user show page" do
         post :create, :session => @attr
         response.should redirect_to(user_path(@user))
       end
+    end
+  end
+  
+  describe "DELETE 'destroy'" do
+    it "should sign a user out" do
+      test_sign_in(Factory(:user))
+      delete :destroy
+      controller.should not_be_signed_in
+      response.should redirect_to(root_path)
     end
   end
 
